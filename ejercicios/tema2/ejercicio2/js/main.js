1b2ded51c6d8434d27f23f6b1ef34230308b50f2
Suso <suso@MacBook-Pro-de-Suso.local>
2018-10-09 19:08:43 +0200

    fixed .DS_Store exercise 2

---
+++ ejercicios/tema2/ejercicio2/js/main.js
@@ -0,0 +1,15 @@
+/**
+Modificar el primer script del capítulo anterior para que:
+    1. El mensaje que se muestra al usuario se almacene en una variable llamada mensaje y elfuncionamiento del script sea el mismo.
+
+Autor: Jesús Mejias Leiva
+*/
+
+{
+
+    let mensaje = "Hola mundo!! \n" + 
+                "Qué fácil es incluir 'comillas simples'\n" +
+                " y \"comillas dobles\" ";
+
+    alert(mensaje);
+}
